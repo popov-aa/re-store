701a56c679c0aa2ec54dc6309c0ef14ae0e8c713
Попов Алексей <a.popov@ians.aero>
2019-08-07 15:12:57 +0300

    140

--- src/store.js
+++ src/store.js
@@ -1,5 +1,19 @@
-import {createStore, compose} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import reducer from "./reducers";
+
+const logMiddleware = (store) => (next) => (action) => {
+    console.log(action.type);
+    return next(action);
+};
+
+const stringMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        });
+    }
+    return next(action);
+};
 
 const logEnhancer = (createStore) => (...args) => {
     const store = createStore(...args);
@@ -9,7 +23,7 @@ const logEnhancer = (createStore) => (...args) => {
         return originalDispatch(action);
     }
     return store;
-}
+};
 
 const stringEnhancer = (createStore) => (...args) => {
     const store = createStore(...args);
@@ -21,9 +35,10 @@ const stringEnhancer = (createStore) => (...args) => {
         return originalDispatch(action);
     }
     return store;
-}
+};
 
 
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+// const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
 store.dispatch('HELLO_WORLD');
 export default store;
